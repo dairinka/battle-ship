1e0e57b365cc98f31eecde4457ca1c3de63a768f
dairinka <interest4me@gmail.com>
2023-07-08 22:57:10 +0300

    feat: add count websockets

--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -1,16 +1,27 @@
 import Websocket, { WebSocketServer } from 'ws';
 import { parseUserData } from '../processing/parseUserData';
+let wsId = 0;
+const connectedWs = new Map<number, Websocket>();
 
 export const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', (ws: Websocket) => {
   console.log('Websocket connection');
-
+  wsId = wsId + 1;
+  const connectId = wsId;
+  connectedWs.set(connectId, ws);
   ws.on('message', (buffer: Buffer) => {
     console.log('msg from user: ', JSON.parse(buffer.toString()));
-    parseUserData(JSON.parse(buffer.toString()));
+    const serverAnswer = parseUserData(JSON.parse(buffer.toString()), connectId);
 
-    ws.send(JSON.stringify({ message: 'Received your msg' }));
+    console.log('info from server: ', JSON.stringify(serverAnswer));
+    ws.send(JSON.stringify(serverAnswer));
+
+    connectedWs.forEach((connect) => {
+      if (connect !== ws && serverAnswer.type === 'update_room') {
+        connect.send(JSON.stringify(serverAnswer));
+      }
+    });
   });
 
   ws.on('close', () => {
