64ccac00f5064fa8993d80d515deced3aa946183
dairinka <interest4me@gmail.com>
2023-07-15 19:11:44 +0300

    fix: correct types in gameRoom database

--- src/database/GameRooms.ts
+++ src/database/GameRooms.ts
@@ -1,29 +1,38 @@
-import {
-  IUserInfo,
-  IAddPlayerToRoomRequest,
-  IUpdateRoomStateResponse,
-  IAddShipRequest,
-} from '../types/types';
+import { IUserInfo, IAddPlayerToRoomRequest, IUpdateRoomStateResponse } from '../types/types';
 
 interface IUserInfoInRoom {
   user_1: [userName: string, userId: number, wsId: number];
   user_2?: [userName: string, userId: number, wsId: number];
 }
-
+type roomNumber = number;
+type idGame = number;
 class GameRooms {
-  listRooms: string[];
-  roomInfo: Map<string, IUserInfoInRoom>;
-  idGame: number[];
+  listRooms: number[];
+  roomInfo: Map<roomNumber, IUserInfoInRoom>;
+  idGame: { [key: roomNumber]: idGame };
+  countRoom: number;
 
   constructor() {
-    this.listRooms = ['room_1', 'room_2', 'room_3', 'room_4'];
+    this.listRooms = [100, 101, 102, 103, 104, 105, 106, 107, 108];
     this.roomInfo = new Map();
-    this.idGame = [12, 13, 14, 15, 16];
+    this.idGame = {
+      100: 12,
+      101: 13,
+      102: 14,
+      103: 15,
+      104: 16,
+      105: 17,
+      106: 18,
+      107: 19,
+      108: 20,
+    };
+    this.countRoom = 0;
   }
 
   createRoom(userInfo: IUserInfo, wsId: number): IUpdateRoomStateResponse[] {
     const roomNumber = this.roomInfo.size;
-    this.roomInfo.set(`room_${roomNumber}`, {
+
+    this.roomInfo.set(this.listRooms[0], {
       user_1: [userInfo.name, userInfo.index, wsId],
     });
 
@@ -32,14 +41,14 @@ class GameRooms {
   }
 
   addUserToRoom({ indexRoom }: IAddPlayerToRoomRequest, wsId: number, userInfo: IUserInfo) {
-    const roomName = `room_${indexRoom}`;
-    const room = this.roomInfo.get(roomName);
-    if (room && room.user_1[0] !== userInfo.name) {
-      room.user_2 = [userInfo.name, userInfo.index, wsId];
+    const roomInfo = this.roomInfo.get(indexRoom);
+
+    if (roomInfo && roomInfo.user_1[0] !== userInfo.name && roomInfo.user_2 === undefined) {
+      roomInfo.user_2 = [userInfo.name, userInfo.index, wsId];
 
       Array.from(this.roomInfo.entries()).forEach(([key, otherRoom]) => {
         if (
-          key !== roomName &&
+          key !== indexRoom &&
           (otherRoom.user_1[0] === userInfo.name ||
             (otherRoom.user_2 && otherRoom.user_2[0] === userInfo.name))
         ) {
@@ -47,10 +56,11 @@ class GameRooms {
             otherRoom.user_2 = undefined;
           } else {
             this.roomInfo.delete(key);
-            this.listRooms = this.listRooms.filter((el) => el !== key);
           }
         }
       });
+      this.listRooms = this.listRooms.slice(1);
+
       return {
         idGame: this.idGame[indexRoom],
         idPlayer: userInfo.index,
@@ -61,8 +71,7 @@ class GameRooms {
   updateRoom(): IUpdateRoomStateResponse[] {
     return Array.from(this.roomInfo.entries())
       .filter(([_, room]) => room.user_2 === undefined)
-      .map(([roomName, room]) => {
-        const roomId = Number(roomName.split('_')[1]);
+      .map(([roomId, room]) => {
         return {
           roomId,
           roomUsers: [
@@ -74,22 +83,31 @@ class GameRooms {
         };
       });
   }
-  getWsIdsinOneRoomByRoomIndex(roomIndex: number): number[] {
-    const roomName = `room_${roomIndex}`;
 
-    const currentRoom = this.roomInfo.get(roomName);
+  getWsIdsinOneRoomByRoomIndex(roomIndex: number): number[] {
+    const currentRoom = this.roomInfo.get(roomIndex);
 
     return [currentRoom.user_1[2], currentRoom.user_2[2]];
   }
-  getUserIdsinOneRoomByRoomIndex(roomIndex: number): number[] {
-    const roomName = `room_${roomIndex}`;
 
-    const currentRoom = this.roomInfo.get(roomName);
+  getUserIdsinOneRoomByRoomIndex(roomIndex: number): number[] {
+    const currentRoom = this.roomInfo.get(roomIndex);
 
     return [currentRoom.user_1[1], currentRoom.user_2[1]];
   }
 
-  addShips(userData: IAddShipRequest) {}
+  getRoomNumberByIdGame(idGame: number): roomNumber {
+    for (const key in this.idGame) {
+      if (this.idGame[key] === idGame) {
+        return parseInt(key);
+      }
+    }
+  }
+  closeRoom(roomIndex: number): void {
+    delete this.idGame[roomIndex];
+
+    this.roomInfo.delete(roomIndex);
+  }
 }
 
 export const currentGameRooms = new GameRooms();
